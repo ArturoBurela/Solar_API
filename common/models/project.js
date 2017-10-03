59c6c1ee5dee8d9799f69571d4051254738642d7
Arturo Burela <uno@arturoburela.com>
2017-10-03 09:17:59 -0500

    Project insertion hook

--- common/models/project.js
+++ common/models/project.js
@@ -1,5 +1,16 @@
 'use strict';
+var app = require('../../server/server');
 
 module.exports = function(Project) {
-
+  var MapsAPI = app.datasources.GoogleMaps;
+  Project.observe('before save', function getElevation(ctx, next) {
+    MapsAPI.getElevation("39.6391536,-104.9847034|12.23423,-99.1").then(function(value) {
+      // cumplimiento
+      console.log(value);
+    }, function(reason) {
+      // rechazo
+      console.log(reason);
+    });
+    next();
+  });
 };
