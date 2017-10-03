09f0608aba022237582cdbf77cdf2e8ec7ce3a68
Arturo Burela <uno@arturoburela.com>
2017-10-02 22:46:58 -0500

    add Elevation method

--- server/boot/script.js
+++ server/boot/script.js
@@ -2,24 +2,37 @@
 
 module.exports = function(app) {
   /*
-   * The `app` object provides access to a variety of LoopBack resources such as
-   * models (e.g. `app.models.YourModelName`) or data sources (e.g.
-   * `app.datasources.YourDataSource`). See
-   * http://docs.strongloop.com/display/public/LB/Working+with+LoopBack+objects
-   * for more info.
-   */
-   var Role = app.models.Role;
-   var dataSource = app.datasources.GoogleMaps;
-   //create the admin role
-   Role.create({
-     name: 'admin'
-   }, function(err, role) {
-     if (err) cb(err);
-   });
+  * The `app` object provides access to a variety of LoopBack resources such as
+  * models (e.g. `app.models.YourModelName`) or data sources (e.g.
+  * `app.datasources.YourDataSource`). See
+  * http://docs.strongloop.com/display/public/LB/Working+with+LoopBack+objects
+  * for more info.
+  */
+  var Role = app.models.Role;
+  var dataSource = app.datasources.GoogleMaps;
+  //create the admin role
+  Role.create({
+    name: 'admin'
+  }, function(err, role) {
+    if (err) cb(err);
+  });
 
-   function lol(result) {
-     console.log(result);
-   }
+  function lol(result) {
+    console.log(result);
+  }
 
-   dataSource.geocode('107 S B St', 'San Mateo', '94401', lol);
+  dataSource.geocode('107 S B St', 'San Mateo', '94401').then(function(value) {
+    // cumplimiento
+    console.log(value);
+  }, function(reason) {
+    // rechazo
+    console.log(reason);
+  });
+  dataSource.getElevation("39.6391536,-104.9847034|12.23423,-99.1").then(function(value) {
+    // cumplimiento
+    console.log(value);
+  }, function(reason) {
+    // rechazo
+    console.log(reason);
+  });
 };
