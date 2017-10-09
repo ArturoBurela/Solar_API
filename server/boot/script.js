cbd6bcb6e35c470eac9935c156f14b6756b3cf1f
Arturo Burela <uno@arturoburela.com>
2017-10-09 15:31:29 -0500

    Add blobstore

--- server/boot/script.js
+++ server/boot/script.js
@@ -3,18 +3,20 @@ module.exports = function (app) {
   var Role = app.models.Role;
   //automigrar todos los modelos a base de datos en postgresql
   app.datasources.SolarDB.autoupdate(function(err, result){
-    console.log("Performed automigration");
-    if(err) throw err;
-  });
-  //crear Role
-  Role.find({ name: 'admin' }, function(err, results) {//Encontrar el Role
-    if (err) { throw err; }
-
-    if (results.length < 1) {//si no existe entonces crearlo
-      Role.create({
-        name: 'admin'
-      }, function(err, role) {
-        if (err) throw err;
+    if(err){
+      throw err;
+    }else{
+      console.log("Performed automigration");
+      //crear Role
+      Role.find({ name: 'admin' }, function(err, results) {//Encontrar el Role
+        if (err) { throw err; }
+        if (results.length < 1) {//si no existe entonces crearlo
+          Role.create({
+            name: 'admin'
+          }, function(err, role) {
+            if (err) throw err;
+          });
+        }
       });
     }
   });
@@ -33,4 +35,8 @@ module.exports = function (app) {
 
     console.log(body);
   });
+
+  //var AWS = require('aws-sdk');
+
+
 }
