cd2d4f7f1987811eab6258748d41f833920d2ba5
Arturo Burela <uno@arturoburela.com>
2017-10-11 17:43:45 -0500

    node schedule

--- server/boot/script.js
+++ server/boot/script.js
@@ -1,18 +1,20 @@
 'use strict';
-module.exports = function (app) {
+module.exports = function(app) {
   var Role = app.models.Role;
-  //automigrar todos los modelos a base de datos en postgresql
-  app.datasources.SolarDB.autoupdate(function(err, result){
-    if(err){
+  // automigrar todos los modelos a base de datos en postgresql
+  app.datasources.SolarDB.autoupdate(function(err, result) {
+    if (err) {
       throw err;
-    }else{
-      console.log("Performed automigration");
-      //crear Role
-      Role.find({ name: 'admin' }, function(err, results) {//Encontrar el Role
-        if (err) { throw err; }
-        if (results.length < 1) {//si no existe entonces crearlo
+    } else {
+      console.log('Performed automigration');
+      // crear Role
+      Role.find({name: 'admin'}, function(err, results) { // Encontrar el Role
+        if (err) {
+          throw err;
+        }
+        if (results.length < 1) { // si no existe entonces crearlo
           Role.create({
-            name: 'admin'
+            name: 'admin',
           }, function(err, role) {
             if (err) throw err;
           });
@@ -20,41 +22,58 @@ module.exports = function (app) {
       });
     }
   });
-  //Get token from UAA to use Analytics Framework
-  var request = require("request");
-  var options = { method: 'POST',
-  url: 'https://8dc085ff-272e-4cac-901e-15c3f90233ee.predix-uaa.run.aws-usw02-pr.ice.predix.io/oauth/token',
-  headers:
-  { 'cache-control': 'no-cache',
-  'content-type': 'application/x-www-form-urlencoded',
-  authorization: 'Basic YWZfcnQ6cGFzc3dvcmQ=' },
-  form: { client_id: 'af_rt', grant_type: 'client_credentials' } };
-  request(options, function (error, response, body) {
+  // Get token from UAA to use Analytics Framework
+  var request = require('request');
+  var options = {
+    method: 'POST',
+    url: 'https://8dc085ff-272e-4cac-901e-15c3f90233ee.predix-uaa.run.aws-usw02-pr.ice.predix.io/oauth/token',
+    headers:
+    {
+      'cache-control': 'no-cache',
+      'content-type': 'application/x-www-form-urlencoded',
+      authorization: 'Basic YWZfcnQ6cGFzc3dvcmQ=',
+    },
+// eslint-disable-next-line camelcase
+    form: {client_id: 'af_rt', grant_type: 'client_credentials'},
+  };
+  request(options, function(error, response, body) {
     if (error) throw new Error(error);
     var UAAresponse = JSON.parse(body);
     console.log(UAAresponse);
-    //List available Analytics
-    ///api/v1/catalog/analytics
+    // List available Analytics
+    /// api/v1/catalog/analytics
     var data = JSON.parse('{"data": {"plant_data": {"tilt": 5, "system_capacity": 1000, "array_azimuth": 0}, "system_data": {"inverter_data": {"inverter_efficiency": 0.9, "inverter_losses": 0.05}, "pv_module_data": {"array_losses": 0.05, "pv_module_efficiency": 0.11, "noct": 45.0, "temperature_coefficient": 0.004}}, "resource_data": {"latitude": 51.3, "air_temperature": [-0.2, 0.8, 3.9, 7.9, 12.6, 15.6, 17.2, 16.9, 13.5, 9.3, 4.1, 1.1], "daily_solar_radiation": [0.67, 1.39, 2.28, 3.69, 4.83, 4.78, 4.81, 4.08, 2.81, 1.56, 0.81, 0.47]}}, "config": {}}');
     console.log(UAAresponse.access_token);
     console.log(data);
-    options = { method: 'POST',
-    url: 'https://predix-analytics-catalog-release.run.aws-usw02-pr.ice.predix.io/api/v1/catalog/analytics/aa7bbd17-23c0-4bf1-8a53-3ddd13267f68/execution',
-    headers:
-    { 'Predix-Zone-Id': '00c30eb0-8b5e-411c-bc3f-9d3a5d70f0d0',
-    'content-type': 'application/json',
-    authorization: 'Bearer ' + UAAresponse.access_token},
-  json: data};
-  console.log(options);
-    request(options, function (error, response, body) {
+    options = {
+      method: 'POST',
+      url: 'https://predix-analytics-catalog-release.run.aws-usw02-pr.ice.predix.io/api/v1/catalog/analytics/aa7bbd17-23c0-4bf1-8a53-3ddd13267f68/execution',
+      headers:
+      {
+        'Predix-Zone-Id': '00c30eb0-8b5e-411c-bc3f-9d3a5d70f0d0',
+        'content-type': 'application/json',
+        authorization: 'Bearer ' + UAAresponse.access_token,
+      },
+      json: data,
+    };
+    request(options, function(error, response, body) {
       if (error) throw new Error(error);
       console.log(body);
     });
   });
 
-  //Set REST API Blobstore
-  //require('request').debug = true;
-  /*var ak = "AKIAIVS7MIZMEOIIC34Q";
+  var schedule = require('node-schedule');
+  var date = new Date(2012, 11, 21, 5, 30, 0);
+  var j = schedule.scheduleJob(date, function(){
+    console.log('The world is going to end today.');
+  });
+
+  // var Project = app.models.Project;
+  // Project.run();
+
+  // Set REST API Blobstore
+  // require('request').debug = true;
+  /* var ak = "AKIAIVS7MIZMEOIIC34Q";
   var bn = "bucket-16909670-8588-4793-a261-2ffc3a64d3c4";
   var host = "s3-us-west-2.amazonaws.com";
   var sak = "vUnSsLZm8F/f+V2cBqFL+cCESDYBhnorZ9heHLLD";
@@ -63,25 +82,25 @@ module.exports = function (app) {
   var string = 'GET\n\n\n'+date.toUTCString()+'\n/'+bn+'/';
   console.log(string);
   var crypto = require('crypto');
-  var hash = crypto.createHmac('sha1', sak).update(string).digest('base64');*/
-  /*var string2 = 'GET\nelasticmapreduce.amazonaws.com\n/\nAWSAccessKeyId=AKIAIOSFODNN7EXAMPLE&Action=DescribeJobFlows&SignatureMethod=HmacSHA256&SignatureVersion=2&Timestamp=2011-10-03T15%3A19%3A30&Version=2009-03-31';
+  var hash = crypto.createHmac('sha1', sak).update(string).digest('base64'); */
+  /* var string2 = 'GET\nelasticmapreduce.amazonaws.com\n/\nAWSAccessKeyId=AKIAIOSFODNN7EXAMPLE&Action=DescribeJobFlows&SignatureMethod=HmacSHA256&SignatureVersion=2&Timestamp=2011-10-03T15%3A19%3A30&Version=2009-03-31';
   var hash2 = crypto.createHmac('sha256', 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY').update(string2).digest('base64');
   */
-  /*options = {
+  /* options = {
     method: 'GET',
     url: url,
     headers: {
       'Date': date.toUTCString(),
       'Authorization':' AWS '+ak+':'+hash
     }
-  };*/
-  /*request(options, function (error, response, body) {
+  }; */
+  /* request(options, function (error, response, body) {
     if (error) throw new Error(error);
     console.log(body);
-  });*/
+  }); */
 
-  //Import app object
-  /*var app = require('../../server/server');
+  // Import app object
+  /* var app = require('../../server/server');
   //Import GoogleMapsAPI as a datasource
   var BlobStore = app.datasources.BlobStore;
   var BlobStoreConnector = BlobStore.connector;
@@ -98,6 +117,5 @@ module.exports = function (app) {
   }, function(reason) {
     //If elevation cant be obtained respond with error
     console.log(reason);
-  });*/
-
-}
+  }); */
+};
