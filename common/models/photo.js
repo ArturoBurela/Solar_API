22eb8e93496f56a585391f14784b80acca128b60
Arturo Burela <uno@arturoburela.com>
2017-11-04 19:42:30 -0600

    Full new blostore methods

--- common/models/photo.js
+++ common/models/photo.js
@@ -6,74 +6,93 @@ module.exports = function(Photo) {
   var s3 = new AWS.S3();
   // Bucket names must be unique across all S3 users
   var myBucket = process.env.AWS_BLOBSTORE_BUCKET;
-  var myKey = 'myBucketKey';
   var params;
 
-  function getPhoto() {
-    params = {Bucket: myBucket, Key: myKey};
+  function getPhoto(key, callback) {
+    params = {Bucket: myBucket, Key: key.toString()};
     s3.getObject(params, function(err, data) {
-      if (err) console.log(err, err.stack);
-      else {
-        console.log(data.Body.toString());
-        return data.Body.toString();
+      if (err) {
+        console.log(err, err.stack);
+        callback(err);
+      } else {
+        callback(data.Body.toString());
       }
     });
-  };
+  }
 
-  function savePhoto() {
-    params = {Bucket: myBucket, Key: myKey, Body: 'Hello!'};
+  function savePhoto(key, data, callback) {
+    params = {Bucket: myBucket, Key: key.toString(), Body: data};
     s3.putObject(params, function(err, data) {
       if (err) {
-        console.log(err);
+        console.log(err, err.stack);
+        callback(err);
       } else {
-        console.log('Successfully uploaded data to myBucket/myKey');
+        callback(true);
       }
     });
-  };
-  function listPhotos() {
+  }
+
+  function listPhotos(callback) {
     var params = {
       Bucket: myBucket,
       MaxKeys: 10,
     };
     s3.listObjects(params, function(err, data) {
-      if (err) console.log(err, err.stack);
-      else     console.log(data);
+      if (err) {
+        console.log(err, err.stack);
+        callback(err);
+      } else {
+        console.log(data);
+        callback(data);
+      }
     });
   }
-  // USE CALLBACSK OR OTHER SHIT TO RETURN VALUE
-  function deletePhoto() {
-    params = {Bucket: myBucket, Key: myKey};
+
+  function deletePhoto(key, callback) {
+    params = {Bucket: myBucket, Key: key.toString()};
     s3.deleteObject(params, function(err, data) {
-      if (err) console.log(err, err.stack); // an error occurred
-      else     console.log(data);           // successful response
+      if (err) {
+        console.log(err, err.stack);
+        callback(err);
+      } else {
+        callback(data);
+      }
     });
-  };
+  }
+
   // Set hooks to access and save photos
-  Photo.afterRemote('find', function(ctx, photo, next) {
-    console.log(photo);
+  /* Photo.afterRemote('find', function(ctx, photo, next) {
     if (!!photo) {
       // Obtener imagen desde el blobstore
-      photo.image = getPhoto();
+      getPhoto(photo.id, function(err, data) {
+        photo.image = data;
+        next();
+      });
+    } else {
+      next();
     }
-    next();
-  });
+  }); */
   Photo.afterRemote('findById', function(ctx, photo, next) {
-    // listPhotos();
-    console.log(photo);
     if (!!photo) {
       // Obtener imagen desde el blobstore
-      photo.image = getPhoto();
+      getPhoto(photo.id, function(data) {
+        photo.image = data;
+        next();
+      });
+    } else {
+      next();
     }
-    next();
   });
-  /* Photo.observe('before save', function(ctx, next) {
-    savePhoto();
-    console.log(ctx);
-    next();
+  Photo.observe('after save', function(ctx, next) {
+    // Guardar imagen en blobstore
+    savePhoto(ctx.instance.id, ctx.instance.image, function() {
+      next();
+    });
   });
   Photo.observe('before delete', function(ctx, next) {
-    deletePhoto();
-    console.log(ctx);
-    next();
-  }); */
+    // Delete image from blobstore
+    deletePhoto(ctx.where.id, function() {
+      next();
+    });
+  });
 };
